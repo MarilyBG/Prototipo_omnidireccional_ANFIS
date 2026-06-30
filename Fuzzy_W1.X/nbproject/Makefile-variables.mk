75b245d03403c967e89f8171226c06845f0bd7d0
Marilyn Barrero <90068383+MarilyBG@users.noreply.github.com>
2026-06-29 21:22:27 -0500

    Add files via upload

---
+++ Fuzzy_W1.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Fuzzy_W1.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Fuzzy_W1.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=fuzzyw1.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/fuzzyw1.x.tar
